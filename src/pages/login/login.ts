f2b33a2bc7c9a0ce1efefd0e41546f1c6669649b
HOANGTHIEN\thien <hoangthien.uit@gmail.com>
2017-03-07 18:21:34 +0700

    add LoginPage and get token

---
+++ src/pages/login/login.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+
+@Component({
+	selector: 'page-login',
+	templateUrl: 'login.html'
+})
+export class LoginPage {
+	registerCredentials = {username: '', password: ''};
+
+	constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+
+	login() {
+		var token = btoa(this.registerCredentials.username + ':' + this.registerCredentials.password);
+
+		var headers = new Headers();
+		headers.append('Authorization', 'Basic ' + token);
+		headers.append('Content-Type', 'application/hal+json');
+		headers.append('Accept', 'application/json');
+		
+		console.log('111111');
+		console.log(headers);
+		console.log(token);
+		console.log(this.registerCredentials);
+	}
+}
